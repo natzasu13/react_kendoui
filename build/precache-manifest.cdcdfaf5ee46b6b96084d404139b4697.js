29019387eb20678eec042ec3acb984ff2ed01062
Natalia Zartha Suarez <natalia.ti@onoff.com.co>
2019-11-23 14:22:05 -0500

    add build

---
+++ build/precache-manifest.cdcdfaf5ee46b6b96084d404139b4697.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67b7d7a5224fdd0900a98023dc7b9f9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07f59a79cf690a9a3e28",
+    "url": "/static/js/2.9b8347b6.chunk.js"
+  },
+  {
+    "revision": "aab1d9dd01a1a16b268c",
+    "url": "/static/js/main.56333767.chunk.js"
+  },
+  {
+    "revision": "0bcb1a52d4a21e3eb847",
+    "url": "/static/js/runtime-main.4832192e.js"
+  }
+]);
